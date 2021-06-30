20da796556fe7f9e2a893c2349afda6b8e118600
ing-diaz30 <84823408+ing-diaz30@users.noreply.github.com>
2021-06-29 21:58:46 -0700

    FormulaMatematica

---
+++ FormulaMatematica.py
@@ -0,0 +1,10 @@
+formulas ={
+    "Pitagora":"a^2´b^2=c^2",
+    "Albert Einstein":"E=mc^2",
+    "Area Cuadrado":"A=L^2",
+    "Area Circulo":"A=r^2*3.1416",
+    "Area Rectangulo":"A=base*altura"
+}
+
+ingresefor=input("Escriba una formula: ")
+print("La formula de {} es {} ".format(ingresefor,formulas[ingresefor]))
